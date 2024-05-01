7fc805b7c17b56f93d86c1792ab0e2ee08061ad0
Richard Hadzhiev <ri4koto100@gmail.com>
2024-05-01 16:44:40 +0200

    implement display list items and a list component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import Footer from './components/Footer.jsx'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Sidebar from './components/Sidebar.jsx'
+import ListItem from "./components/ListItem.jsx"
 
 function App() {
+  
 
   return (
     <>
     <Navbar />
+    <ListItem />
     <Footer />
     <Sidebar />
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 function Navbar() {
     return (
         <div className="navbar">
-    <h1>Mini E-Commerce</h1>
-    <img src='https://freevector-images.s3.amazonaws.com/uploads/vector/preview/36682/36682.png' width={182} height={182} />
+    <img src="./logo.png" />
     </div>
     )
 }
